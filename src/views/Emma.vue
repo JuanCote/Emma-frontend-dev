957e37f16e25f6e880fe454fa1d345ec64f15aa3
JuanCote <lyy1707@gmail.com>
2024-04-19 18:18:01 +0300

    change ws /

--- src/views/Emma.vue
+++ src/views/Emma.vue
@@ -4,7 +4,7 @@ import EmmaAnalytic from "@/components/Emma/EmmaAnalytic.vue"
 import EmmaRightContainer from "@/components/Emma/EmmaRightContainer.vue"
 
 import { BACKEND_URL_WS } from '@/config.js'
-
+import { BASE_URL } from '@/config.js'
 
 export default {
     data() {
@@ -19,8 +19,8 @@ export default {
         EmmaRightContainer
     },
     beforeRouteEnter(to, from, next) {
-        if (to.path === `/emma`) {
-            next(`/emma/analytic`);
+        if (to.path === `${BASE_URL}/emma`) {
+            next(`${BASE_URL}/emma/analytic`);
         } else {
             next();
         }
@@ -30,7 +30,7 @@ export default {
             this.chatsLoaded = true
         })
         this.$store.dispatch('checkUser').then(() => {
-            this.socket = new WebSocket(`${BACKEND_URL_WS}/ws/`);
+            this.socket = new WebSocket(`${BACKEND_URL_WS}/ws`);
             this.socket.onopen = () => {
                 const data = {
                     type: 'manager',
